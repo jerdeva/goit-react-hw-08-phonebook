958ae0dab60c631d677c9a68003ac1967bfc988e
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-10-20 16:54:14 +0300

    login and home pages added

---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+export default function Home (){
+    <div>
+      <h1>
+        Task manager welcome page
+        <span role="img" aria-label="Greeting icon">
+          💁‍♀️
+        </span>
+      </h1>
+    </div>;
+}
