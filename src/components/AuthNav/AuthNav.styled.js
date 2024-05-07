e9b0132ed6a0dcebd460cebf09aacf1064a61ba8
Olena Jerdeva <lena.jerdeva28@gmail.com>
2024-05-07 16:45:53 +0300

    style correction

--- src/components/AuthNav/AuthNav.styled.js
+++ src/components/AuthNav/AuthNav.styled.js
@@ -6,11 +6,9 @@ import { NavLink } from 'react-router-dom';
 export const Link = styled(NavLink)`
     display: inline-block;
     text-decoration: none;
-    padding: 12px;
-    font-weight: 500;
-    /* color: #2a363b; */
+    font-weight: 700;
     font-size: 28px;
-    margin: 32px 46px 0 0;
+    margin: 0px 46px 0 0;
     color: #EF3959;
     :active{
     color: #e84a5f;
